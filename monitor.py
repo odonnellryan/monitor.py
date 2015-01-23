4f1c3bceed914cd853448b92645008ed807bfa7b
odonnellryan <odonnellryanc@gmail.com>
2015-01-23 06:26:45 -0500

    .gitignore

--- monitor.py
+++ monitor.py
@@ -1,28 +1,23 @@
-from multiprocessing import Process, Manager
+from multiprocessing import Process, Queue
 from time import sleep
 
-class BaseMonitor:
+class BaseMonitor(Process):
     def __init__(self):
+        super().__init__()
         self.data_source = []
         self.query_interval = 0
 
 class BooleanMonitor(BaseMonitor):
-    def __init__(self, data):
-        self.data = data
+    def __init__(self, data_queue):
+        self.data_queue = data_queue
         super().__init__()
 
     def register(self, data_source: (str, bool)):
         self.data_source.append(data_source)
 
-    def _monitor(self, d):
+    def run(self):
         while True:
-            d['boolean_monitor'] = [func() for func in self.data_source]
-            #print(self.data)
+            if not self.data_queue.empty(): self.data_queue.get()
+            self.data_queue.put([i for func in self.data_source for i in func()])
+            print(self.data_source)
             sleep(self.query_interval)
-
-    def start(self):
-        self.p = Process(target=self._monitor, args=(self.data,))
-        self.p.start()
-
-    def stop(self):
-        self.p.stop()
--- test.py
+++ test.py
@@ -1,16 +1,18 @@
-from monitor import BooleanMonitor, BaseMonitor, Manager
+from monitor import BooleanMonitor, BaseMonitor, Queue
 
-def test_func() -> (str, bool):
-    return [("testing", True),("testing", True)]
+def test_func():
+    return [("testing", True), ("1", False)]
 
-def test_func2() -> (str, bool):
+def test_func2():
     return test_func()
 
 if __name__ == '__main__':
-    d = Manager().dict()
-    email_monitor = BooleanMonitor(d)
+    q = Queue()
+    email_monitor = BooleanMonitor(q)
     email_monitor.query_interval = 1
     email_monitor.register(test_func)
     email_monitor.register(test_func2)
     email_monitor.start()
+    while True:
+        print(q.get())
     #print(email_monitor.data)
