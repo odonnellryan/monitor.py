46756d37106be9b6f59973f7339b1de74301a59b
odonnellryanc <odonnellryanc@gmail.com>
2015-01-26 14:27:56 -0500

    need to define how to represent the backup schedule

--- email_monitor.py
+++ email_monitor.py
@@ -2,19 +2,40 @@ from monitor import BooleanMonitor, BaseMonitor
 import monitor_manager
 import requests
 import config
+import json
+
+folder_id = {'Old': 'AAMkADlmY2IwYTFkLWMxNDAtNGIyNy04NDM0LTJhMDQ4NTc2YmYyZAAuAAAAAAAofRrKZvRgQ4RoTUPPf3uJAQDvpmjL_PTZTLP1WRdlp2cYAAAB6fBfAAA='}
+
+class BackupSchedule:
+
+
+class BackupMonitor:
+    def __init__(self):
 
 def get_emails():
     r = requests.get('https://outlook.office365.com/api/v1.0/me/messages', auth=(config.o365['email'], config.o365['password']))
     return r.json()
 
 def move_emails(emails):
+    """
+    Move each email that's passed into the Old folder.
+    :param emails: dict
+    :return: error or
+    """
     for email in emails['value']:
-        print(email['Id'])
-        data = {'DestinationId':'Old'}
-        r = requests.post('https://outlook.office365.com/api/v1.0/me/messages/{0}/move'.format(email['Id']), data=data,
-                         auth=(config.o365['email'], config.o365['password']))
-        print(r)
-    print(emails)
+        data = {'DestinationId': folder_id['Old']}
+        headers = {'Content-Type': 'Application/Json'}
+        r = requests.post(email['@odata.id'] + '/move', data=json.dumps(data),
+                         auth=(config.o365['email'], config.o365['password']), headers=headers)
+        return r.json()
 
+def get_folders():
+    """
+    helper function to return the folders on o365 (so we can get the folder id for later use)
+    :return:
+    """
+    r = requests.get('https://outlook.office365.com/api/v1.0/me/folders', auth=(config.o365['email'], config.o365['password']))
+    return r.json()
 
+#print(get_folders())
 print(move_emails(get_emails()))
